64b161c471ec7cc75d530dbebd560ed5dc69340a
Widley Mayk <widleywmss@gmail.com>
2025-04-04 19:09:47 -0300

    Delete questao3.py

--- questao3.py
+++
@@ -1,9 +0,0 @@
-
-numero_um = float(input("Digite o primeiro número: "))
-numero_dois = float(input("Digite o segundo número: "))
-
-soma = numero_um + numero_dois
-
-
-print("A soma dos dois números é:", soma)
-
